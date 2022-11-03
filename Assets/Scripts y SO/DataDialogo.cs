af3947eb6d74361a72ca00aac704c4b223cb1ec5
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-11-03 09:00:21 -0300

    Added the things for dialogue and Scriptable Objects

---
+++ Assets/Scripts y SO/DataDialogo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPCData", menuName = "SO NPC Data")]
+
+public class DataDialogo : ScriptableObject
+{
+   public string[] LineasDialogo;
+}
---
+++ Assets/Scripts y SO/ManejanteDialogo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManejanteDialogo : MonoBehaviour
+{
+    [SerializeField] GameObject UIElements;
+    [SerializeField] Text dialogueTxt;
+    [SerializeField] string[] NPCDialogue;
+    [SerializeField] NPCDialogo NPCDialogueScript;
+    int dialogueIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        NPCDialogueScript = other.gameObject.GetComponent<NPCDialogo>();
+        if (NPCDialogueScript)
+        {
+            UIElements.SetActive(true);
+            NPCDialogue = NPCDialogueScript.data.LineasDialogo;
+            ShowNextDialogue();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            UIElements.SetActive(false);
+            dialogueIndex = 0;
+        }
+    }
+
+    public void ShowNextDialogue()
+    {
+        if (dialogueIndex <= NPCDialogue.Length)
+        {
+            dialogueTxt.text = NPCDialogue[dialogueIndex];
+            dialogueIndex++;
+        }
+    }
+}
