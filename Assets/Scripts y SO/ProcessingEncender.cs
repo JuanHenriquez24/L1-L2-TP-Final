3a20eade178fb7e853730462e7afef68b3863b10
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-11-15 15:22:37 -0300

    Added stuff

---
+++ Assets/Scripts y SO/ProcessingEncender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcessingEncender : MonoBehaviour
+{
+    public DataJugadorYObjetos data;
+    public GameObject Proccess;
+    // Update is called once per frame
+    void Start()
+    {
+        Proccess.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (data.startTimer == true)
+        {
+            Proccess.SetActive(true);
+        }
+    }
+}
